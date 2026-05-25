b13f83821f1ae51da565a02e563999edca411e3e
nabatikeju1 <bart.playgame10@gmail.com>
2026-05-25 19:52:11 +0700

    done

--- OrderList.java
+++ OrderList.java
@@ -3,10 +3,10 @@ class OrderList {
     
       void addOrder(Order o) {
         NodeOrder newNode = new NodeOrder(o);
-        if (head == null) {
+        if (head == null) { // if only one node then head and tail will point to the same node
             head = tail = newNode;
         } else {
-            tail.next = newNode;
+            tail.next = newNode; // if theres some new node then the next of tail will point to new node
             newNode.prev = tail;
             tail = newNode;
         }
@@ -15,10 +15,10 @@ class OrderList {
 
      void sortOrdersByName() {
         if (head == null) return;
-        for (NodeOrder i = head; i != null; i = i.next) {
+        for (NodeOrder i = head; i != null; i = i.next) { // i stand in front of j, and j will compare with i until j is null
             for (NodeOrder j = i.next; j != null; j = j.next) {
-                if (i.data.orderName.compareToIgnoreCase(j.data.orderName) > 0) {
-                    Order temp = i.data;
+                if (i.data.orderName.compareToIgnoreCase(j.data.orderName) > 0) { 
+                    Order temp = i.data; // Swap the order data of nodes i and j
                     i.data = j.data;
                     j.data = temp;
                 }
--- QueueBuyer.java
+++ QueueBuyer.java
@@ -3,12 +3,12 @@ class QueueBuyer {
     int count = 0;
 
     void addQueue(String name, String phoneNumber){
-        Buyer b = new Buyer(name, phoneNumber);
+        Buyer b = new Buyer(name, phoneNumber); // create a new buyer and give ++ticket number
         NodeBuyer newNode = new NodeBuyer(++count, b);
-        if (head == null) {
+        if (head == null) { // if only one node then head and tail will point to the same node
             head = tail = newNode;
         } else {
-            tail.next = newNode;
+            tail.next = newNode; // if theres some new node then the next of tail will point to new node
             newNode.prev = tail;
             tail = newNode;
         }
@@ -18,7 +18,7 @@ class QueueBuyer {
     void printQueue(){
         NodeBuyer temp = head;
         System.out.println("===== QUEUE LIST =====");
-        while (temp != null) {
+        while (temp != null) { // while temp is not null, then temp will point to the next node until temp is null
             System.out.println("Queue Number : " + temp.queueNumber);
             System.out.println("Buyer Name   : " + temp.data.name);
             System.out.println("Phone Number : " + temp.data.phoneNumber);
@@ -29,12 +29,12 @@ class QueueBuyer {
     }
 
     NodeBuyer removeQueue(){
-     if (head == null) return null;
+     if (head == null) return null;// if head is null, then there is no queue to remove
         NodeBuyer served = head;
         head = head.next;
-        if (head != null) head.prev = null;
+        if (head != null) head.prev = null; //if head is not null, then the previous of head will point to null. 
         else tail = null;
-        count--;
+        count--; // 5. Decrease queue count and return the served customer data
         return served;
     }
 }
